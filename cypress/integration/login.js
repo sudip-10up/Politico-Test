cf1960ed9491f633711ecc29b296a86a3529300c
sudip-10up <sudip.dadhaniya@10up.com>
2022-02-10 17:50:27 +0530

    Setup

---
+++ cypress/integration/login.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+describe('Politico',()=>{
+
+    beforeEach(()=>{
+
+        cy.admin_login('sudip.dadhaniya','qXynA(WloQf7guyamWrm23ld')
+        
+    })
+    it('Dashboard', ()=> {
+         
+    })
+})
